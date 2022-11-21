6f2a94f637805fadde0dc1760f2b1ba403018d03
Datamining <111824123+Faridghozali@users.noreply.github.com>
2022-11-21 19:23:20 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,68 @@
 import pickle
 import streamlit as st
  
+  html_temp = """ 
+    		<header id="header" class="" data-scroll-index="0">
+
+			<div id="header-wrap">
+
+				<div class="container">
+					<div class="row">
+						<div class="col-md-12">
+							<a class="logo logo-header" href="#">
+								<img src="static/logo1.png" data-logo-alt="static/logo1.png" alt="">
+								<h3><span class="colored">Gajiku</span></h3>
+								<span>HTML Template</span>
+							</a><!-- .logo end -->
+							<div class="header-menu-and-meta">
+								<div class="header-meta">
+									<nav>
+										<a href="biodata.html">About me</a>
+									</nav>
+								</div><!-- .header-meta end -->								
+								<!-- <div class="clearfix"></div> -->
+							</div><!-- .header-menu-and-meta end -->
+
+						</div><!-- .col-md-12 end -->
+					</div><!-- .row end -->
+				</div><!-- .container end -->
+
+			</div><!-- #header-wrap end -->
+
+		</header><!-- #header end -->
+
+		<!-- Banner
+		============================================= -->
+		<section id="banner" data-scroll-index="0">
+
+			<div class="banner-parallax">
+				<img src="images/files/parallax-bg/img-1.jpg" alt="">
+				<div class="overlay-colored" data-bg-color="#000" data-bg-color-opacity="0.65"></div><!-- .overlay-colored end -->
+				<div class="slide-content">
+
+					<div class="container">
+						<div class="row">
+							<div class="col-md-10 col-md-offset-1">
+
+								<div class="banner-center-box text-white text-center">
+									<h1>Diabetes adalah penyakit kronis yang ditandai dengan tingginya kadar gula darah. </h1>
+									<div class="description">
+                    Glukosa merupakan sumber energi utama bagi sel tubuh manusia. Akan tetapi, pada penderita diabetes, glukosa tersebut tidak dapat digunakan oleh tubuh. Kadar gula (glukosa) dalam darah dikendalikan oleh hormon insulin yang diproduksi pankreas. Namun, pada penderita diabetes, pankreas tidak mampu memproduksi insulin sesuai kebutuhan tubuh. Tanpa insulin, sel-sel tubuh tidak dapat menyerap dan mengolah glukosa menjadi energi.
+										<br>
+									</div>
+									<a class="scroll-to btn xx-large colorful hover-dark mt-30" href="#our-services">syarat dan kriteria</a>
+								</div><!-- .banner-center-box end -->
+
+							</div><!-- .col-md-10 end -->
+						</div><!-- .row end -->
+					</div><!-- .container end -->
+
+				</div><!-- .slide-content end -->
+			</div><!-- .banner-parallax end -->
+
+		</section><!-- #banner end -->
+    """
+  
 # loading the trained model
 pickle_in = open('classifier.pkl', 'rb') 
 classifier = pickle.load(pickle_in)
