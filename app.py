b2e8368b66d458d30636ae96566d3602704d80b7
Datamining <111824123+Faridghozali@users.noreply.github.com>
2022-11-22 00:59:52 +0700

    Update app.py

--- app.py
+++ app.py
@@ -63,7 +63,7 @@ def Biodata():
 page_names_to_funcs = {
     "Home": Home,
     "Page 2": Data,
-    "Page 3": page3,
+    "Page 3": Biodata,
 }
 
 selected_page = st.sidebar.selectbox("Select a page", page_names_to_funcs.keys())
