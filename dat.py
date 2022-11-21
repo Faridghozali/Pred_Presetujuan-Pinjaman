7b1ad57da9f8f18ffa8ef093e1f06922fec6bdc9
Datamining <111824123+Faridghozali@users.noreply.github.com>
2022-11-22 00:12:07 +0700

    Rename data.py to dat.py

--- dat.py
+++ dat.py
@@ -3,7 +3,7 @@ import streamlit as st
 st.set_page_config(
     page_title="Code-Project",
     page_icon="👋",
-    )
+)
 
 st.title("Halo Semuanya !👋")
 st.write('Kamu Bisa menemukan Source Code Project ini')
