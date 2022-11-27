27eb79ced1cc4e3304b45ec4b8caa716e4ddff5c
Datamining <111824123+Faridghozali@users.noreply.github.com>
2022-11-27 20:10:49 +0700

    Update app.py

--- app.py
+++ app.py
@@ -45,19 +45,6 @@ st.set_page_config(
     page_icon="💵",
 )
 st.image("home.png")
-col1, col2, col3 = st.columns(3)
-
-with col1:
-   st.header("A cat")
-   st.image("https://static.streamlit.io/examples/cat.jpg")
-
-with col2:
-   st.header("A dog")
-   st.image("https://static.streamlit.io/examples/dog.jpg")
-
-with col3:
-   st.header("An owl")
-   st.image("https://static.streamlit.io/examples/owl.jpg")
   
 # this is the main function in which we define our webpage  
 def main():       
@@ -86,6 +73,19 @@ def main():
         print(LoanAmount)
     st.title("FAQ > Masalah Pinjaman")
     st.image('info.png')
+col1, col2, col3 = st.columns(3)
+
+with col1:
+   st.header("A cat")
+   st.image("https://static.streamlit.io/examples/cat.jpg")
+
+with col2:
+   st.header("A dog")
+   st.image("https://static.streamlit.io/examples/dog.jpg")
+
+with col3:
+   st.header("An owl")
+   st.image("https://static.streamlit.io/examples/owl.jpg")
       
 if __name__=='__main__': 
     main()
