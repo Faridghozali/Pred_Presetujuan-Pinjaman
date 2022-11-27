09cf057addf95c53188717d8fb92dd173055f65b
Datamining <111824123+Faridghozali@users.noreply.github.com>
2022-11-27 19:06:22 +0700

    Update app.py

--- app.py
+++ app.py
@@ -45,10 +45,19 @@ st.set_page_config(
     page_icon="💵",
 )
 
-def Home():
-    st.markdown("# Home🏠")
-    st.sidebar.markdown("# Home🏠")
-    st.image('home.png')
+tab1, tab2, tab3 = st.tabs(["Home", "Data", "Biodata"])
+
+with tab1:
+   st.header("A cat")
+   st.image('home.png')
+
+with tab2:
+   st.header("A dog")
+   st.image("https://static.streamlit.io/examples/dog.jpg", width=200)
+
+with tab3:
+   st.header("An owl")
+   st.image("https://static.streamlit.io/examples/owl.jpg", width=200)
     # this is the main function in which we define our webpage  
 def main():       
     # front end elements of the web page 
