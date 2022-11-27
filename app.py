11ce4c81698e9b97ef50da072924b78290f38417
Datamining <111824123+Faridghozali@users.noreply.github.com>
2022-11-27 20:16:29 +0700

    Update app.py

--- app.py
+++ app.py
@@ -73,19 +73,17 @@ def main():
         print(LoanAmount)
         col1, col2, col3 = st.columns(3)
 
-with col1:
-   st.header("A cat")
-   st.image("https://static.streamlit.io/examples/cat.jpg")
+        with col1:
+           st.header("A cat")
+           st.image("https://static.streamlit.io/examples/cat.jpg")
 
-with col2:
-   st.header("A dog")
-   st.image("https://static.streamlit.io/examples/dog.jpg")
+       with col2:
+          st.header("A dog")
+          st.image("https://static.streamlit.io/examples/dog.jpg")
 
-with col3:
-   st.header("FAQ > Masalah Pinjaman")
-   st.image('info.png')
-
+      with col3:
+         st.header("FAQ > Masalah Pinjaman")
+         st.image('info.png')
       
 if __name__=='__main__': 
     main()
-  
