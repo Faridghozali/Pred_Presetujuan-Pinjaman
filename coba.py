ae56b1407c2da26561640b6eefb313e74e4c97e4
Datamining <111824123+Faridghozali@users.noreply.github.com>
2022-11-27 21:13:31 +0700

    Update coba.py

--- coba.py
+++ coba.py
@@ -2,14 +2,7 @@ import pickle
 import streamlit as st
 import pandas as pd
 import numpy as np
-  
-# loading the trained model
-pickle_in = open('classifier.pkl', 'rb') 
-classifier = pickle.load(pickle_in)
- 
-@st.cache()
-  
-# defining the function which will make the prediction using the data which the user inputs 
+
 
 st.sidebar.header("Pilihan kategori")
 #Upluad file CSV Untuk pilihan kategori
@@ -18,86 +11,11 @@ if upload_file is not none:
   inputan=pd.read_csv(upload_file)
 else :
   def prediction():
-      Gender=st.sidebar.selectbox('Gender',('pria','wanita')
-      Married=st.sidebar.selectbox(
-      ApplicantIncome=st.sidebar.selectbox(
+      Gender=st.sidebar.selectbox('Gender',('male','female'))
+      Married=st.sidebar.selectbox('Marital Status',('Unmarried','Maried'))
+      ApplicantIncome=st.sidebar.selectbox('Applicants monthly income',('
       LoanAmount=st.sidebar.selectbox(
-      Credit_History=st.sidebar.selectbox(
+      Credit_History=st.sidebar.selectbox('Credit_History',('Unclear Debts','Clear Debts'))
     (Gender, Married, ApplicantIncome, LoanAmount, Credit_History):
     
     
-    # Pre-processing user input    
-    if Gender == "Male":
-        Gender = 0
-    else:
-        Gender = 1
- 
-    if Married == "Unmarried":
-        Married = 0
-    else:
-        Married = 1
- 
-    if Credit_History == "Unclear Debts":
-        Credit_History = 0
-    else:
-        Credit_History = 1  
- 
-    LoanAmount = LoanAmount / 1000
-  
- 
-    # Making predictions 
-    prediction = classifier.predict( 
-        [[Gender, Married, ApplicantIncome, LoanAmount, Credit_History]])
-     
-    if prediction == 0:
-        pred = 'Rejected'
-    else:
-        pred = 'Approved'
-    return pred
-  
-st.set_page_config(
-    page_title="Prediksi Pinjaman",
-    page_icon="💵",
-)
-    
-st.image("home.png")
-col1, col2= st.columns(2)
-
-with col1:
-   st.header("Data Peminjam Online")
-   st.write("https://github.com/Faridghozali/Pred_Presetujuan-Pinjaman/blob/main/prediksiloan.csv")
-
-with col2:
-   st.header("A dog")
-   st.image("https://static.streamlit.io/examples/dog.jpg")
-
-# this is the main function in which we define our webpage  
-def main():       
-    # front end elements of the web page 
-    html_temp = """ 
-    <div style ="background-color:yellow;padding:13px"> 
-    <h1 style ="color:black;text-align:center;">Streamlit Loan Prediction ML App</h1> 
-    </div> 
-    """
-      
-    # display the front end aspect
-    st.markdown(html_temp, unsafe_allow_html = True) 
-      
-    # following lines create boxes in which user can enter data required to make prediction 
-    Gender = st.selectbox('Gender',("Male","Female"))
-    Married = st.selectbox('Marital Status',("Unmarried","Married")) 
-    ApplicantIncome = st.number_input("Applicants monthly income") 
-    LoanAmount = st.number_input("Total loan amount")
-    Credit_History = st.selectbox('Credit_History',("Unclear Debts","No Unclear Debts"))
-    result =""
-      
-    # when 'Predict' is clicked, make the prediction and store it 
-    if st.button("Predict"): 
-        result = prediction(Gender, Married, ApplicantIncome, LoanAmount, Credit_History) 
-        st.success('Your loan is {}'.format(result))
-        print(LoanAmount)
-        st.write("FAQ > Masalah Pinjaman")
-        st.image('info.png')
-      
-if __name__=='__main__': 
-    main()
