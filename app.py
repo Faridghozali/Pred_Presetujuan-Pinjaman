9f0ad9dbb8f98b1c8374bbf9da42d571bcc774e6
Datamining <111824123+Faridghozali@users.noreply.github.com>
2022-11-27 22:31:36 +0700

    Update app.py

--- app.py
+++ app.py
@@ -45,14 +45,6 @@ st.set_page_config(
 )
 
 st.image("home.png")
-txt = st.text_area('Text to analyze', '''
-    It was the best of times, it was the worst of times, it was
-    the age of wisdom, it was the age of foolishness, it was
-    the epoch of belief, it was the epoch of incredulity, it
-    was the season of Light, it was the season of Darkness, it
-    was the spring of hope, it was the winter of despair, (...)
-    ''')
-st.write('Sentiment:', run_sentiment_analysis(txt))
 st.sidebar.header("kategori")
 
 # this is the main function in which we define our webpage  
@@ -90,6 +82,14 @@ with col1:
 
 with col2:
    st.header("A dog")
-   st.image("https://static.streamlit.io/examples/dog.jpg")  
+   st.image("https://static.streamlit.io/examples/dog.jpg") 
+txt = st.text_area('Text to analyze', '''
+    It was the best of times, it was the worst of times, it was
+    the age of wisdom, it was the age of foolishness, it was
+    the epoch of belief, it was the epoch of incredulity, it
+    was the season of Light, it was the season of Darkness, it
+    was the spring of hope, it was the winter of despair, (...)
+    ''')
+st.write('Sentiment:', run_sentiment_analysis(txt))
 if __name__=='__main__': 
     main()
