7db422695951d1a8a76730990142dd81844ca1de
Datamining <111824123+Faridghozali@users.noreply.github.com>
2022-11-27 20:12:42 +0700

    Update app.py

--- app.py
+++ app.py
@@ -74,18 +74,17 @@ def main():
     st.title("FAQ > Masalah Pinjaman")
     st.image('info.png')
     col1, col2, col3 = st.columns(3)
+    with col1:
+      st.header("A cat")
+      st.image("https://static.streamlit.io/examples/cat.jpg")
+      
+   with col2:
+     st.header("A dog")
+     st.image("https://static.streamlit.io/examples/dog.jpg")
 
-with col1:
-   st.header("A cat")
-   st.image("https://static.streamlit.io/examples/cat.jpg")
-
-with col2:
-   st.header("A dog")
-   st.image("https://static.streamlit.io/examples/dog.jpg")
-
-with col3:
-   st.header("An owl")
-   st.image("https://static.streamlit.io/examples/owl.jpg")
+  with col3:
+    st.header("An owl")
+    st.image("https://static.streamlit.io/examples/owl.jpg")
       
 if __name__=='__main__': 
     main()
