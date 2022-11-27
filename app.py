f2beb5c8b1120ce87c1313f9c0dfab7b09a741f6
Datamining <111824123+Faridghozali@users.noreply.github.com>
2022-11-27 19:35:13 +0700

    Update app.py

--- app.py
+++ app.py
@@ -44,9 +44,20 @@ st.set_page_config(
     page_title="Prediksi Pinjaman",
     page_icon="💵",
 )
-add_selectbox = st.sidebar.selectbox(
-    "How would you like to be contacted?",
-    ("Email", "Home phone", "Mobile phone")
+col1, col2, col3 = st.columns(3)
+
+with col1:
+   st.header("A cat")
+   st.image("https://static.streamlit.io/examples/cat.jpg")
+
+with col2:
+   st.header("A dog")
+   st.image("https://static.streamlit.io/examples/dog.jpg")
+
+with col3:
+   st.header("An owl")
+   st.image("https://static.streamlit.io/examples/owl.jpg")
+  
     # this is the main function in which we define our webpage  
 def main():       
     # front end elements of the web page 
