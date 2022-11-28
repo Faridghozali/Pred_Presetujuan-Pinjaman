ac6bd277d836fca2bbc8c9d2836dafcaf8158673
Datamining <111824123+Faridghozali@users.noreply.github.com>
2022-11-28 16:28:29 +0700

    Update app.py

--- app.py
+++ app.py
@@ -79,7 +79,8 @@ def main():
         result = prediction(Gender, Married, ApplicantIncome, LoanAmount, Credit_History) 
         st.success('Your loan is {}'.format(result))
         print(LoanAmount)
-        st.image("footer.png")
+       st.image("info.png")
+      st.header("FAQ > Masalah App")
 
 if __name__=='__main__': 
     main()
